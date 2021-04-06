71ba34d09e04070c99bcc57bde2302a2d620d9fb
ricassiocosta <ricassio_costa@outlook.com>
2021-04-06 18:57:59 -0300

    creating initial structure to CarImages

---
+++ src/shared/infra/typeorm/migrations/1617741651167-CreateCarImages.ts
@@ -0,0 +1,52 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateCarImages1617741651167 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cars_image',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'car_id',
+            type: 'uuid',
+          },
+          {
+            name: 'image_name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'specifications_cars',
+      new TableForeignKey({
+        name: 'FKCarImage',
+        referencedTableName: 'cars',
+        referencedColumnNames: ['id'],
+        columnNames: ['car_id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('cars_image');
+  }
+}
