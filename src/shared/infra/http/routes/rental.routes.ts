a33b0c14dbef43a970f30af40afdfa63a1742690
ricassiocosta <ricassio_costa@outlook.com>
2021-04-16 17:20:26 -0300

    listing rentals by user

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/listRentalsByUserController';
 import { Router } from 'express';
 
 import { ensureAuthentication } from '../middlewares/ensureAuthentication';
@@ -8,12 +9,19 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post('/', ensureAuthentication, createRentalController.handle);
 rentalRoutes.post(
   '/devolution/:id',
   ensureAuthentication,
   devolutionRentalController.handle
+);
+
+rentalRoutes.get(
+  '/user',
+  ensureAuthentication,
+  listRentalsByUserController.handle
 );
 
 export { rentalRoutes };
