f80eef7c0f770666286cb5cc6c712ef4b98d4f35
ricassiocosta <ricassio_costa@outlook.com>
2021-04-17 20:33:08 -0300

    password rescue use case

--- src/shared/providers/DateProvider/IDateProvider.ts
+++ src/shared/providers/DateProvider/IDateProvider.ts
@@ -3,6 +3,7 @@ interface IDateProvider {
   compareInDays(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
   addDays(days: number): Date;
+  addHours(hours: number): Date;
 }
 
 export { IDateProvider };
